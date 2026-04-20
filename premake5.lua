a92504fa9ee632178f83d4cc94b0208d5221fc49
metadevv <164035951+themetadevv@users.noreply.github.com>
2026-04-20 19:43:25 +0545

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,23 +1,25 @@
 project "YAML"
 	kind "StaticLib"
 	language "C++"
-  cppdialect "C++20"
+    cppdialect "C++20"
 	staticruntime "off" 
 
-  targetdir ("bin/yaml/builds/%{cfg.system}_%{cfg.buildcfg}")
-  objdir ("bin/yaml/intermediates/%{cfg.system}")
+	targetdir ("bin/yaml/builds/%{cfg.system}_%{cfg.buildcfg}")
+	objdir ("bin/yaml/intermediates/%{cfg.system}")
 
 	includedirs
-  {
-      "include"
-  }
+  	{
+      	"include"
+  	}
 
-  files
-  {
-      "include/**.h",
-      "src/**.h",
-      "src/**.cpp"
-  }
+	files
+	{
+	    "include/**.h",
+	    "src/**.h",
+	    "src/**.cpp"
+	}
+
+	defines { "YAML_CPP_STATIC_DEFINE" }
 
 	filter "system:windows"
 		systemversion "latest"
