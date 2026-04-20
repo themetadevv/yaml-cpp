348bc6a742ab78d8b0178a7254fd7fe599dee4d6
metadevv <164035951+themetadevv@users.noreply.github.com>
2026-04-20 19:33:47 +0545

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "YAML"
+	kind "StaticLib"
+	language "C++"
+  cppdialect "C++20"
+	staticruntime "off" 
+
+  targetdir ("bin/yaml/builds/%{cfg.system}_%{cfg.buildcfg}")
+  objdir ("bin/yaml/intermediates/%{cfg.system}")
+
+	includedirs
+  {
+      "include"
+  }
+
+  files
+  {
+      "include/**.h",
+      "src/**.h",
+      "src/**.cpp"
+  }
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		optimize "off"
+
+	filter "configurations:Release"
+		runtime "Release"
+		symbols "off"
+		optimize "on"
